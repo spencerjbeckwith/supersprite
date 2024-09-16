3dca962de98b715d2d65f1728d60b5936a0b942c
Spencer Beckwith <spencerjbeckwith@gmail.com>
2024-09-16 00:52:04 -0700

    Compositor occupation sets/checks

--- src/atlas/Compositor.spec.ts
+++ src/atlas/Compositor.spec.ts
@@ -4,6 +4,12 @@ import { Gatherer } from "./gatherers/Gatherer";
 import { SpriteSource } from "./sources/SpriteSource";
 import { SpriteData } from "../types";
 import sinon from "sinon";
+import path from "path";
+import Jimp from "jimp";
+import fs from "fs";
+
+const temp = ".tmp-Compositor";
+const cleanup = true; // Set to false if you want to view any output files after running tests
 
 /** Random integer between `min` and `max` */
 function randomInt(min = 1, max = 10) {
@@ -63,6 +69,9 @@ describe("Compositor", () => {
     let logStub: sinon.SinonStub;
     before(() => {
         logStub = sinon.stub(console, "log");
+        if (!fs.existsSync(temp)) {  
+            fs.mkdirSync(temp);
+        }
     });
 
     afterEach(() => {
@@ -71,6 +80,12 @@ describe("Compositor", () => {
 
     after(() => {
         logStub.restore();
+        if (cleanup) {
+            fs.rmSync(temp, {
+                force: true,
+                recursive: true,
+            });
+        }
     });
 
     it("gathers and reads every sprite source", async () => {
@@ -156,6 +171,71 @@ describe("Compositor", () => {
         const c = new Compositor(new FakeGatherer(), false);
         await c.load();
         expect(logStub.called).toBe(false);
+    });
+
+    it("determines column ranges for checking occupation", () => {
+        const c = new Compositor();
+
+        // null granularityX (defaults to using 1)
+        let result = c.getOccupiedColumnRange(0, 20);
+        expect(result.start).toBe(0);
+        expect(result.end).toBe(19);
+
+        result = c.getOccupiedColumnRange(8, 4);
+        expect(result.start).toBe(8);
+        expect(result.end).toBe(11);
+
+        // set granularityX
+        c.granularityX = 4;
+        result = c.getOccupiedColumnRange(4, 16);
+        expect(result.start).toBe(1);
+        expect(result.end).toBe(4);
+
+        result = c.getOccupiedColumnRange(6, 12);
+        expect(result.start).toBe(1);
+        expect(result.end).toBe(4);
+
+        result = c.getOccupiedColumnRange(2, 2);
+        expect(result.start).toBe(0);
+        expect(result.end).toBe(0);
+    });
+
+    it("initializes columns when getting a column range", () => {
+        const c = new Compositor();
+        c.granularityX = 4;
+        expect(c.occupied.length).toBe(0);
+        const result = c.getOccupiedColumnRange(4, 16);
+        expect(c.occupied.length).toBe(result.end);
+        for (let i = 0; i < c.occupied.length; i++) {
+            expect(c.occupied[i]).toBe(0);
+        }
+    });
+
+    it("checks all relevant columns when checking occupation", () => {
+        const c = new Compositor();
+        c.granularityX = 4;
+        c.occupied = [0, 8, 0];
+        expect(c.isOccupied(0, 0, 12)).toBe(true);
+        expect(c.isOccupied(0, 8, 12)).toBe(false);
+    });
+
+    it("updates all relevant columns when setting occupation", () => {
+        const c = new Compositor();
+        c.granularityX = 4;
+        c.setOccupied(0, 12, 12, 12);
+        expect(c.occupied[0]).toBe(24);
+        expect(c.occupied[1]).toBe(24);
+        expect(c.occupied[2]).toBe(24);
+        expect(c.isOccupied(0, 24, 8)).toBe(false);
+        expect(c.isOccupied(2, 20, 2)).toBe(true);
+    });
+
+    it("saves atlas image to the specified path", async () => {
+        const c = new Compositor();
+        const p = path.join(temp, "atlas.png");
+        const j = new Jimp(4, 4, 0);
+        await c.saveAtlas(p, j);
+        expect (fs.existsSync(p)).toBe(true);
     });
 
     // ...
--- src/atlas/Compositor.ts
+++ src/atlas/Compositor.ts
@@ -47,11 +47,30 @@ export class Compositor {
     /** If this Compositor may print to the console with `Compositor.log()`. Defaults to true. */
     logEnabled: boolean;
 
+    /** 
+     * Indicates which areas of the atlas which are already occupied.
+     * 
+     * Each column of `granularityX` width is represented by each index of the array.
+     * The value of the array indicates the number below which *that column* is occupied by sprites.
+     * Therefore, any Y pixel value greater than or equal to the value at that index is unoccupied.
+     * Note that that just refers to one column - if placing a sprite greater than `granularityX` width,
+     * multiple columns must be checked.
+     * 
+     * For convenience, use the `setOccupied` and `isOccupied` functions of this class.
+     * 
+     * Atlas width is determined by the length of this array times `granularityX`,
+     * while atlas height is determined by the maximum value of any index in the array.
+     * 
+     * This array is expected to expand while mapping image positions in the atlas.
+     */
+    occupied: number[];
+
     constructor(gatherer?: Gatherer, logEnabled = true) {
         this.gatherer = gatherer;
         this.granularityX = null;
         this.granularityY = null;
         this.logEnabled = logEnabled;
+        this.occupied = [];
     }
 
     /** Load all sprite data from the Gatherer */
@@ -83,24 +102,67 @@ export class Compositor {
         return [...sprites].sort((s1, s2) => (s1.width * s1.height) - (s2.width * s2.height));
     }
 
-    /** Returns if a rectangle of the provided size may be mapped into the atlas at the given location */
-    isOccupied(x: number, y: number, width: number, height: number): boolean {
-        // TODO
-        throw new CompositorError("Not implemented");
+    /** Returns the start and end column indices, inclusive, that should be checked for placing a sprite with the given pixel width at the given pixel X position. */
+    getOccupiedColumnRange(x: number, width: number): { start: number, end: number } {
+        const gx = this.granularityX ?? 1;
+        const result = {
+            start: Math.floor(x / gx),
+            end: Math.floor((x + width - 1) / gx),
+        };
+        // If we go outside our current range, extend the occupied array
+        if (result.end > this.occupied.length) {
+            const padding = new Array(result.end - this.occupied.length);
+            padding.fill(0);
+            this.occupied.push(...padding);
+        }
+        return result;
     }
 
-    /** Sets the atlas status for a region to be occupied and prevent other images being placed on top */
+    /** Returns if a rectangle of the provided size may be mapped into the atlas at the given pixel location */
+    isOccupied(x: number, y: number, width: number): boolean {
+        const { start, end } = this.getOccupiedColumnRange(x, width);
+        for (let i = start; i <= end; i++) {
+            if (this.occupied[i] > y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Sets the atlas status for a region to be occupied at a pixel location and prevent other images being placed on top */
     setOccupied(x: number, y: number, width: number, height: number) {
-        // TODO
-        throw new CompositorError("Not implemented");
+        const { start, end } = this.getOccupiedColumnRange(x, width);
+        for (let i = start; i <= end; i++) {
+            this.occupied[i] = y + height;
+        }
     }
 
     /** Determine where in the atlas to place sprites, according to their sizes */
     map(sprites: SpriteData[]): AtlasMap {
-        if (this.granularityX === null|| this.granularityY === null) {
+        if (this.granularityX === null || this.granularityY === null) {
             this.setGranularity(sprites);
         }
-        // TODO
+    
+        // cx/cy: current X and Y in our atlas. gx/gy: granularity (smallest pixel distance between sprites)
+        let cx = 0;
+        let cy = 0;
+        const gx = this.granularityX;
+        const gy = this.granularityY;
+
+        // Empty our array from the front
+        const remaining = [...sprites];
+        while (remaining.length > 0) {
+            const place = remaining.shift();
+
+            // What'll be the best way to lay out our sprites?
+            // We can go an arbitrary distance vertically before we move along horizontally,
+            // so what is the end goal? Do we want a square-ish atlas?
+            // Why not just make the atlas very long and thin?
+            // Maybe we should set an arbitary max-height and just go from there
+
+            // ...
+        }
+
         throw new CompositorError("Not implemented");
     }
 
@@ -112,8 +174,7 @@ export class Compositor {
 
     /** Writes a composed atlas to an image file */
     async saveAtlas(path: string, atlas: Jimp) {
-        // TODO
-        throw new CompositorError("Not implemented");
+        await atlas.writeAsync(path);
     }
 
     /** Writes the atlas map (of sprite data and precomputed texture matrices) to a file */
